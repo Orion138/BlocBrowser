ad09e3d9f962a9ae1efcdfaeea1506992010b35f
Nathan Boosinger <nathan.boosinger@gmail.com>
2015-11-20 01:01:49 -0600

    first commit

---
+++ BlocBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocBrowser
+//
+//  Created by Nathan Boosinger on 11/20/15.
+//  Copyright (c) 2015 Nathan Boosinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
